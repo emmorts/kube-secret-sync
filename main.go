25aa9f426fd01939f965d06abd42b65ee3858cf7
Tomas Stropus <h.guzas@gmail.com>
2024-01-11 16:12:12 +0200

    Refactoring

--- main.go
+++ main.go
@@ -18,35 +18,54 @@ import (
 	"k8s.io/klog/v2"
 )
 
-var (
-	secretName          string
-	sourceNamespace     string
-	targetImage         string
-	processedNamespaces sync.Map
+const (
+	envSecretName      = "SECRET_NAME"
+	envSourceNamespace = "SOURCE_NAMESPACE"
+	envTargetImage     = "TARGET_IMAGE"
 )
 
-func init() {
-	secretName = os.Getenv("SECRET_NAME")
-	if secretName == "" {
-		klog.Fatalf("SECRET_NAME environment variable not set")
-	}
+type Config struct {
+	secretName      string
+	sourceNamespace string
+	targetImage     string
+}
 
-	sourceNamespace = os.Getenv("SOURCE_NAMESPACE")
-	if sourceNamespace == "" {
-		klog.Fatalf("SOURCE_NAMESPACE environment variable not set")
-	}
-
-	targetImage = os.Getenv("TARGET_IMAGE")
-	if targetImage == "" {
-		klog.Fatalf("TARGET_IMAGE environment variable not set")
-	}
-
-	klog.InitFlags(nil)
+type PodController struct {
+	clientset *kubernetes.Clientset
+	queue     workqueue.RateLimitingInterface
+	config    Config
+	processed sync.Map
 }
 
 func main() {
-	klog.Info("Starting secret clone controller")
+	klog.InitFlags(nil)
+	config := loadConfig()
+	klog.Info("Starting secret clone controller...")
 
+	clientset := createClientSet()
+	queue := workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "pods")
+	controller := NewPodController(clientset, queue, config)
+
+	controller.Run()
+}
+
+func loadConfig() Config {
+	return Config{
+		secretName:      getEnvOrFatal(envSecretName),
+		sourceNamespace: getEnvOrFatal(envSourceNamespace),
+		targetImage:     getEnvOrFatal(envTargetImage),
+	}
+}
+
+func getEnvOrFatal(env string) string {
+	value := os.Getenv(env)
+	if value == "" {
+		klog.Fatalf("%s environment variable not set", env)
+	}
+	return value
+}
+
+func createClientSet() *kubernetes.Clientset {
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		klog.Fatalf("Error getting in-cluster config: %v", err)
@@ -56,27 +75,19 @@ func main() {
 	if err != nil {
 		klog.Fatalf("Error creating clientset: %v", err)
 	}
-
-	queue := workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "pods")
-	podController := NewPodController(clientset, queue)
-	podController.Run()
+	return clientset
 }
 
-type PodController struct {
-	clientset *kubernetes.Clientset
-	queue     workqueue.RateLimitingInterface
-}
-
-func NewPodController(clientset *kubernetes.Clientset, queue workqueue.RateLimitingInterface) *PodController {
+func NewPodController(clientset *kubernetes.Clientset, queue workqueue.RateLimitingInterface, config Config) *PodController {
 	return &PodController{
 		clientset: clientset,
 		queue:     queue,
+		config:    config,
 	}
 }
 
 func (c *PodController) Run() {
 	podListWatcher := cache.NewListWatchFromClient(c.clientset.CoreV1().RESTClient(), "pods", metav1.NamespaceAll, fields.Everything())
-
 	_, informer := cache.NewInformer(
 		podListWatcher,
 		&v1.Pod{},
@@ -93,26 +104,30 @@ func (c *PodController) Run() {
 	defer close(stop)
 	go informer.Run(stop)
 
-	for {
-		pod, shutdown := c.queue.Get()
-		if shutdown {
-			break
-		}
-
-		if err := c.processPod(pod.(*v1.Pod)); err != nil {
-			klog.Errorf("Error processing pod: %v", err)
-			c.queue.AddRateLimited(pod)
-		} else {
-			c.queue.Forget(pod)
-		}
-		c.queue.Done(pod)
-	}
+	c.processQueue()
 }
 
 func (c *PodController) enqueuePod(obj interface{}) {
 	pod := obj.(*v1.Pod)
-	if strings.Contains(pod.Spec.Containers[0].Image, targetImage) {
+	if strings.Contains(pod.Spec.Containers[0].Image, c.config.targetImage) {
 		c.queue.Add(pod)
+	}
+}
+
+func (c *PodController) processQueue() {
+	for {
+		obj, shutdown := c.queue.Get()
+		if shutdown {
+			break
+		}
+
+		if err := c.processPod(obj.(*v1.Pod)); err != nil {
+			klog.Errorf("Error processing pod: %v", err)
+			c.queue.AddRateLimited(obj)
+		} else {
+			c.queue.Forget(obj)
+		}
+		c.queue.Done(obj)
 	}
 }
 
@@ -122,37 +137,41 @@ func (c *PodController) processPod(pod *v1.Pod) error {
 		klog.Infof("Processed pod in namespace: %s in %v", pod.Namespace, time.Since(startTime))
 	}()
 
-	klog.Infof("Processing pod in namespace: %s", pod.Namespace)
-	if _, exists := processedNamespaces.Load(pod.Namespace); exists {
+	if _, exists := c.processed.Load(pod.Namespace); exists {
 		return nil
 	}
 
-	if _, err := c.clientset.CoreV1().Secrets(pod.Namespace).Get(context.TODO(), secretName, metav1.GetOptions{}); err == nil {
-		klog.Infof("Secret %s already exists in namespace %s", secretName, pod.Namespace)
+	if err := c.cloneSecretToNamespace(pod.Namespace); err != nil {
+		return err
+	}
+
+	c.processed.Store(pod.Namespace, struct{}{})
+	return nil
+}
+
+func (c *PodController) cloneSecretToNamespace(namespace string) error {
+	if _, err := c.clientset.CoreV1().Secrets(namespace).Get(context.TODO(), c.config.secretName, metav1.GetOptions{}); err == nil {
+		klog.Infof("Secret %s already exists in namespace %s", c.config.secretName, namespace)
 		return nil
 	}
 
-	secret, err := c.clientset.CoreV1().Secrets(sourceNamespace).Get(context.TODO(), secretName, metav1.GetOptions{})
+	secret, err := c.clientset.CoreV1().Secrets(c.config.sourceNamespace).Get(context.TODO(), c.config.secretName, metav1.GetOptions{})
 	if err != nil {
 		klog.Errorf("Failed to get secret from source namespace: %v", err)
 		return err
 	}
 
-	secret.Namespace = pod.Namespace
+	secret.Namespace = namespace
 	secret.ResourceVersion = ""
 	secret.UID = ""
 
-	err = retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		_, err := c.clientset.CoreV1().Secrets(pod.Namespace).Create(context.TODO(), secret, metav1.CreateOptions{})
-		return err
+	return retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		_, err := c.clientset.CoreV1().Secrets(namespace).Create(context.TODO(), secret, metav1.CreateOptions{})
+		if err != nil {
+			klog.Errorf("Failed to clone secret to namespace %s: %v", namespace, err)
+			return err
+		}
+		klog.Infof("Successfully cloned secret to namespace %s", namespace)
+		return nil
 	})
-
-	if err != nil {
-		klog.Errorf("Failed to clone secret to namespace %s: %v", pod.Namespace, err)
-		return err
-	}
-
-	klog.Infof("Successfully cloned secret to namespace %s", pod.Namespace)
-	processedNamespaces.Store(pod.Namespace, struct{}{})
-	return nil
 }
